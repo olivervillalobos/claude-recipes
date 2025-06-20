b7d0a32334a1b7bd27e15fb32711c89643ec18b2
Oliver Villalobos <71286508+olivervillalobos@users.noreply.github.com>
2025-06-20 06:21:27 -0700

    Updates on some CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,35 @@
 export default function Main(){
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const mapIngredients = ingredients.map((ingredient) => {
+        return (
+            <li key={ingredient}>{ingredient}</li>
+        )
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient");
+        ingredients.push(newIngredient);
+        console.log(ingredients);
+    }
+
     return(
         <main className="main-container">
-            <form className="form-addIngredient">
+            <form className="form-addIngredient"onSubmit={handleSubmit}>
                 <input 
                     className="form-input"
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add Ingredient"
+                    name="ingredient"
                 />
                 <button className="form-button">Add Ingredient</button>
             </form>
+            <ul>
+                {mapIngredients}
+            </ul>
         </main>
     )
 }
